f05f247d12ae023cc8ef8963d596357260ae65cd
Anshuma <ansh783@gmail.com>
2015-06-24 22:14:31 +0530

    added Cobertura plugin

---
+++ src/main/java/test/PrintName.java
@@ -0,0 +1,14 @@
+
+package test;
+
+/**
+ * @author a
+ * 
+ */
+public class PrintName {
+
+    public static void main(String args[]) {
+        System.out.println("Name should be printed with this statement");
+    }
+
+}
